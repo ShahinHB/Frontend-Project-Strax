a087c992ab5642c5a3154944df776f0ac7684edb
ShahinHB <72246220+ShahinHB@users.noreply.github.com>
2021-11-10 16:03:30 +0400

    some Changes

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -48,6 +48,10 @@ $(document).ready(function () {
             }
         }
     });
+
+    $(".responsive-header-menu ul .nav i").click(function (e) { 
+        document.querySelector(".responsive-navbar").classList.toggle("show-navbar");
+    });
     // setTimeout(function(){
     //     odometer.innerHTML = 800;
     // });
